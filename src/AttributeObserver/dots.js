df1c005fef71796ae88d9e25427186b414ad1110
orstavik <orstavik77@gmail.com>
2026-08-13 12:23:07 +0200

    suspected typo

--- src/AttributeObserver/dots.js
+++ src/AttributeObserver/dots.js
@@ -1,5 +1,5 @@
 import { EventLoopCube } from "./2_EventLoopCube.js";
-import { PortalsMap } from "./PortalsMap.js";
+import { PortalMap } from "./1_PortalMap.js";
 import { Portals as DomEvents } from "./PortalDomEvents.js";
 import { Portals as WindowDocumentEvents } from "./PortalWindowDocumentEvents.js";
 import { Portals as Observers } from "./PortalObservers.js";
@@ -8,7 +8,7 @@ import { Portals as State } from "./PortalState.js";
 function init() {
   window.EventLoopCube = EventLoopCube;
   window.eventLoopCube = new EventLoopCube();
-  document.portals = new PortalsMap({...DomEvents, ...WindowDocumentEvents, ...Observers, ...State});
+  document.portals = new PortalMap({...DomEvents, ...WindowDocumentEvents, ...Observers, ...State});
 }
 
 document.readyState !== "loading" ? init() : document.addEventListener("DOMContentLoaded", init);
